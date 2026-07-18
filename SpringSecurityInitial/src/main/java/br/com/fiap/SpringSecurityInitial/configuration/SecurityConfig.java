6e56ea43123630bccff7ce1c6e2d3c5d1c02a64c
Tiago Toloni <tiago.toloni@gmail.com>
2026-07-18 17:12:18 -0300

    a

--- SpringSecurityInitial/src/main/java/br/com/fiap/SpringSecurityInitial/configuration/SecurityConfig.java
+++ SpringSecurityInitial/src/main/java/br/com/fiap/SpringSecurityInitial/configuration/SecurityConfig.java
@@ -1,32 +1,20 @@
 package br.com.fiap.SpringSecurityInitial.configuration;
 
-import br.com.fiap.SpringSecurityInitial.service.MyUserDetailService;
 import br.com.fiap.SpringSecurityInitial.support.encoder.PlainTextPasswordEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    private final MyUserDetailService myUserDetailService;
-
-    public SecurityConfig(MyUserDetailService myUserDetailService) {
-        this.myUserDetailService = myUserDetailService;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -34,20 +22,31 @@ public class SecurityConfig {
     }
 
     @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(myUserDetailService);
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(
+                User.withUsername("usuario")
+                        .password("senha")
+                        .roles("USER").build()
+        );
+        manager.createUser(
+                User.withUsername("admin")
+                        .password("senha")
+                        .roles("ADMIN").build()
+        );
+        return manager;
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.userDetailsService(userDetailsService());
+
         http.authorizeHttpRequests(authorizeRequests -> {
             authorizeRequests.requestMatchers("/public").permitAll();
             authorizeRequests.requestMatchers("/logout").permitAll();
             authorizeRequests.anyRequest().authenticated();
         });
-        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
